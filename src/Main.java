d2223629860e3b98c0a8fbfa58fce9d798a5721a
Antony <officialantony23@gmail.com>
2023-02-02 16:59:13 +0300

    intial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean results = hasEqualSum(2,8,5);
+        System.out.println(results);
+    }
+    public static boolean hasEqualSum(int num1,int num2,int num3){
+        int check= num1 + num2;
+        if(check ==num3) {
+            return true;
+        }
+        return false;
     }
 }
